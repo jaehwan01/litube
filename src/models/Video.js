db5ef0a54dce1eb6222b76ebb1a04f7607b4849b
Jaehwan Jung <abcd414141@naver.com>
2021-07-14 12:33:25 +0900

    update_0.01

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,14 +1,26 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  description: { type: String, required: true, trim: true },
+  title: { type: String, required: true, trim: true, maxlength: 20 },
+  description: { type: String, required: true, trim: true, maxlength: 80 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
+});
+
+// videoSchema.pre("save", async function () {
+//   this.hashtags = this.hashtags[0]
+//     .split(",")
+//     .map((word) => (word.startsWith("#") ? word : `#${word}`));
+// });
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
 
 const Video = mongoose.model("Video", videoSchema);
